fc8c033bb1263bdc1fa5846788fc7c067a9f539b
Sven65 <Sven65@users.noreply.github.com>
2023-11-30 11:15:41 +0100

    Add debug line

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,14 @@ RELEASE_FILE=$INPUT_ARTIFACT
 RELEASE_FILE_MIMETYPE=$INPUT_ARTIFACT_MIME_TYPE
 GITHUB_ACCESS_TOKEN=$INPUT_GH_TOKEN
 
+echo "Uploading $RELEASE_FILE to $GITHUB_REPOSITORY with version $RELEASE_NAME"
+
 upload_url=$(curl -s -H "Authorization: Bearer $GITHUB_ACCESS_TOKEN" \
 			"https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/${RELEASE_NAME}" \
 			| jq -r '.upload_url')
 
 upload_url="${upload_url%\{*}"
-          
+
 echo "Uploading release artifact to $upload_url"
           
 curl -X POST -H "Authorization: Bearer $GITHUB_ACCESS_TOKEN" \
